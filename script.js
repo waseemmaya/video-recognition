7d97caff1adc731dd313aa57cabd5c52347138e1
Waseem Maya <waseemmayaa@gmail.com>
2020-06-30 03:08:37 +0500

    random number generator

--- script.js
+++ script.js
@@ -10,8 +10,15 @@ function drawImge() {
   ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 
   var faceArea = 150;
-  var pX = canvas.width / 2 - faceArea / 2;
-  var pY = canvas.height / 2 - faceArea / 2;
+
+  let a = Math.round(Math.random() * 4);
+  console.log("a: ", a);
+
+  var pX = canvas.width / a - faceArea / a;
+  var pY = canvas.height / a - faceArea / a;
+
+  // var pX = canvas.width / 2 - faceArea / 2;
+  // var pY = canvas.height / 2 - faceArea / 2;
 
   ctx.rect(pX, pY, faceArea, faceArea);
   ctx.lineWidth = "6";
@@ -23,9 +30,10 @@ function drawImge() {
 }
 
 function play() {
+  console.log("play");
   var video = document.querySelector("#webCamera");
   video.onplay = function () {
     console.log("onPlay");
-    setTimeout(drawImge, 300);
+    setTimeout(drawImge, 100);
   };
 }
